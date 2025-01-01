8480bf43d0460508b4d9afdb741097cf204229db
Özer Özay <ozerozay@gmail.com>
2025-01-02 02:06:20 +0300

    t

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,17 @@
-import {defineConfig} from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
+            input: ["resources/css/app.css", "resources/js/app.js"],
+            refresh: true
+        })
     ],
     build: {
-        outDir: 'public/tenancy/assets/build',
-        assetsDir: 'assets',
+        outDir: "public/tenancy/assets/build",
+        assetsDir: "assets",
         emptyOutDir: true,
-    },
+        manifest: true
+    }
 });
