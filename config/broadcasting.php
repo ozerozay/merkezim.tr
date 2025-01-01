74c520bb15bad129b68d5feb5c6638ddd29d77bd
Özer Özay <ozerozay@gmail.com>
2025-01-02 02:24:45 +0300

    t

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -10,7 +10,7 @@ return [
     | This option controls the default broadcaster that will be used by the
     | framework when an event needs to be broadcast. You may set this to
     | any of the connections defined in the "connections" array below.
-    |
+    |a
     | Supported: "reverb", "pusher", "ably", "redis", "log", "null"
     |
     */
